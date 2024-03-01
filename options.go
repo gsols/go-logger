df49d1c2efdfd2f7eb9b1d73f518f1afd73ff112
Nick Aguilar <nickaguilarh@gmail.com>
2024-03-01 11:43:39 -0500

    feat: add WithWriter option to setup global writer
    Signed-off-by: Nick Aguilar <nickaguilarh@gmail.com>

--- options.go
+++ options.go
@@ -7,9 +7,12 @@ import (
 )
 
 // Option is a function that takes a pointer to a Logger and sets its options.
+// This is used to apply different configurations to the logger.
 type Option func(logger *zerolog.Logger)
 
-// WithDebug is a function that takes a boolean value and returns an option.
+// WithDebug is a function that returns an option.
+// This option sets the logger's level to Debug.
+// Debug level logs are typically used for development and debugging purposes.
 func WithDebug() Option {
 	return func(l *zerolog.Logger) {
 		*l = l.Level(zerolog.DebugLevel)
@@ -17,6 +20,8 @@ func WithDebug() Option {
 }
 
 // WithVerbosity is a function that takes an integer value and returns an option.
+// This option sets the logger's level based on the provided verbosity.
+// The verbosity is subtracted from the Panic level to determine the log level.
 func WithVerbosity(verbosity int) Option {
 	return func(l *zerolog.Logger) {
 		*l = l.Level(zerolog.Level(int(zerolog.PanicLevel) - verbosity))
@@ -24,6 +29,7 @@ func WithVerbosity(verbosity int) Option {
 }
 
 // WithOutput is a function that takes an io.Writer and returns an option.
+// This option sets the logger's output destination to the provided io.Writer.
 func WithOutput(w io.Writer) Option {
 	return func(l *zerolog.Logger) {
 		*l = l.Output(w)
@@ -31,9 +37,20 @@ func WithOutput(w io.Writer) Option {
 }
 
 // WithSentry is a function that returns an option.
+// This option adds a Sentry hook to the logger.
+// The Sentry hook is used to send logs to a Sentry server for error tracking and monitoring.
 func WithSentry() Option {
 	var sentryHook SentryHook
 	return func(l *zerolog.Logger) {
 		*l = l.Hook(sentryHook)
+	}
+}
+
+// WithWriter is a function that takes an io.Writer and returns an option.
+// This option sets the logger's output destination to the provided io.Writer.
+// This is similar to WithOutput, but can be used when you want to specify a different writer.
+func WithWriter(w io.Writer) Option {
+	return func(l *zerolog.Logger) {
+		*l = l.Output(w)
 	}
 }
