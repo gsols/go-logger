b2fd1451bddaf57cce941c97aab7380f4c66a592
Nick Aguilar <nickaguilarh@gmail.com>
2024-04-19 15:53:50 -0500

    feat: add path to file writer
    Signed-off-by: Nick Aguilar <nickaguilarh@gmail.com>

--- writers.go
+++ writers.go
@@ -3,12 +3,13 @@ package logger
 import (
 	"io"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/rs/zerolog"
 )
 
-const FileNameTimeFormat = "logs/2006-01-02_15_04_05.log"
+const FileNameTimeFormat = "2006-01-02_15_04_05.log"
 
 func WithConsoleWriter() io.Writer {
 	return zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
@@ -17,13 +18,14 @@ func WithConsoleWriter() io.Writer {
 	})
 }
 
-func WithFileWriter() io.Writer {
-	_ = os.MkdirAll("logs", os.ModePerm)
-	file, _ := os.OpenFile(time.Now().Format(FileNameTimeFormat), os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+func WithFileWriter(path string) io.Writer {
+	_ = os.MkdirAll(path, os.ModePerm)
+	path = filepath.Join(path, time.Now().Format(FileNameTimeFormat))
+	file, _ := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 
 	return file
 }
 
-func WithDefaultWriter() io.Writer {
-	return zerolog.MultiLevelWriter(WithConsoleWriter(), WithFileWriter())
+func MultiLevelWriter(writers ...io.Writer) io.Writer {
+	return zerolog.MultiLevelWriter(writers...)
 }
